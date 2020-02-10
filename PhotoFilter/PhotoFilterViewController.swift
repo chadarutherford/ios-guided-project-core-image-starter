d5343359efe305ad061cefd8a02ffa22658a0cb3
Chad Rutherford <chad_rutherford@outlook.com>
2020-02-10 13:01:47 -0500

    Added filter function

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -1,21 +1,43 @@
 import UIKit
 import CoreImage
+import CoreImage.CIFilterBuiltins
 import Photos
 
 class PhotoFilterViewController: UIViewController {
-
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - Outlets
 	@IBOutlet weak var brightnessSlider: UISlider!
 	@IBOutlet weak var contrastSlider: UISlider!
 	@IBOutlet weak var saturationSlider: UISlider!
 	@IBOutlet weak var imageView: UIImageView!
-	
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - Properties
+    private var context = CIContext(options: nil)
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+    // MARK: - View Controller Life Cycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
 	}
-	
-	// MARK: Actions
-	
+    
+    private func filter(_ image: UIImage) -> UIImage? {
+        guard let cgImage = image.cgImage else { return nil }
+        let ciImage = CIImage(cgImage: cgImage)
+        let filter = CIFilter.colorControls()
+        filter.inputImage = ciImage
+        filter.brightness = brightnessSlider.value
+        filter.contrast = contrastSlider.value
+        filter.saturation = saturationSlider.value
+        
+        guard let outputCIImage = filter.outputImage else { return nil }
+        guard let outputCGImage = context.createCGImage(outputCIImage, from: CGRect(origin: .zero, size: image.size)) else { return nil }
+        return UIImage(cgImage: outputCGImage)
+    }
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+	// MARK: - Actions
 	@IBAction func choosePhotoButtonPressed(_ sender: Any) {
 		// TODO: show the photo picker so we can choose on-device photos
 		// UIImagePickerController + Delegate
@@ -24,10 +46,9 @@ class PhotoFilterViewController: UIViewController {
 	@IBAction func savePhotoButtonPressed(_ sender: UIButton) {
 		// TODO: Save to photo library
 	}
-	
-
-	// MARK: Slider events
-	
+    
+    // --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+	// MARK: - Slider events
 	@IBAction func brightnessChanged(_ sender: UISlider) {
 
 	}
@@ -40,4 +61,3 @@ class PhotoFilterViewController: UIViewController {
 
 	}
 }
-
